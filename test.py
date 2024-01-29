e8719c263b517ddd7f42c2f31408f5af57447d1b
Simon Agius Muscat <simon@agius-muscat.net>
2024-01-29 19:37:40 +0100

    Update test script to extract a specific tensor

--- test.py
+++ test.py
@@ -31,3 +31,29 @@ with open(file_path, 'rb') as f:
     json_header = json.loads(f.read(json_header_length))
     pprint(json_header)
     
+
+"""
+Taking an example layer:
+ 'model.layers.7.mlp.down_proj.weight': {'data_offsets': [8634294272,
+                                                          8751734784],
+                                         'dtype': 'BF16',
+                                         'shape': [4096, 14336]},
+"""
+
+with open(file_path, 'rb') as f:
+    header = f.read(8)
+    json_header_length = struct.unpack('Q', header)[0]
+    print(json_header_length)
+    json_header = json.loads(f.read(json_header_length))
+    test_layer = json_header['model.layers.7.mlp.down_proj.weight']
+    print(test_layer)
+    offset_start, offset_end = test_layer['data_offsets']
+    print(offset_start, offset_end)
+    offset_length = offset_end - offset_start
+    print(offset_length)
+    f.seek(offset_start)
+    tensor = f.read(offset_length)
+    print('First 50:')
+    print(tensor[:50])
+    print(f'Tensor is {len(tensor) / 1024.0 / 1024.0} MB large')
+    
