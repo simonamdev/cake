b723b769849de87263f9af97cf76ba7f6837820b
Simon Agius Muscat <simon@agius-muscat.net>
2024-01-30 21:58:56 +0100

    Cache downloaded models to a list to avoid re-attempting them

--- get_models.py
+++ get_models.py
@@ -4,10 +4,23 @@ from huggingface_hub import HfApi, ModelFilter, snapshot_download, utils
 api = HfApi()
 
 download_folder = '/media/simon/models'
+model_list_file_path = './models_downloaded.txt'
+
+if not os.path.isfile(model_list_file_path):
+    open(model_list_file_path, 'w').close()
+
+already_downloaded_model_ids = []
+with open(model_list_file_path, 'r') as f:
+    for line in f:
+        already_downloaded_model_ids.append(
+            line.strip().replace('\n', '')
+        )
 
 models = api.list_models(filter=ModelFilter(tags=['pipeline_tag=text-generation', 'library=safetensors']))
 for model in models:
     print(model.modelId)
+    if model.modelId in already_downloaded_model_ids:
+        continue
     try:
         snapshot_download(
             repo_id=model.modelId,
@@ -17,6 +30,8 @@ for model in models:
             local_dir=os.path.join(download_folder, 'results', model.modelId),
             cache_dir=os.path.join(download_folder, 'cache')
         )
+        with open(model_list_file_path, 'a') as f:
+            f.write(f'{model.modelId}\n')
     except utils._errors.GatedRepoError as e:
         print(e)
         continue
