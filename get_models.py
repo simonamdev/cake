d9da0404c247ed012fb6fddc98c4f3eb2a2f1504
Simon Agius Muscat <simon@agius-muscat.net>
2024-01-30 20:13:00 +0100

    Skip download errors

--- get_models.py
+++ get_models.py
@@ -1,5 +1,5 @@
 import os
-from huggingface_hub import HfApi, ModelFilter, snapshot_download
+from huggingface_hub import HfApi, ModelFilter, snapshot_download, utils
 
 api = HfApi()
 
@@ -8,11 +8,18 @@ download_folder = '/media/simon/models'
 models = api.list_models(filter=ModelFilter(tags=['pipeline_tag=text-generation', 'library=safetensors']))
 for model in models:
     print(model.modelId)
-    snapshot_download(
-        repo_id=model.modelId,
-        repo_type='model',
-        allow_patterns=['*.json', '*.safetensors'],
-        local_dir_use_symlinks=True,
-        local_dir=os.path.join(download_folder, 'results', model.modelId),
-        cache_dir=os.path.join(download_folder, 'cache')
-    )
+    try:
+        snapshot_download(
+            repo_id=model.modelId,
+            repo_type='model',
+            allow_patterns=['*.json', '*.safetensors'],
+            local_dir_use_symlinks=True,
+            local_dir=os.path.join(download_folder, 'results', model.modelId),
+            cache_dir=os.path.join(download_folder, 'cache')
+        )
+    except utils._errors.GatedRepoError as e:
+        print(e)
+        continue
+    except utils._errors.HfHubHTTPError as e:
+        print(e)
+        continue
