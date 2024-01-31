db04734b2201a34384b3f79bbdae71db32446f7b
Simon Agius Muscat <simon@agius-muscat.net>
2024-01-31 23:19:14 +0100

    Define a basic function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
     println!("Hello, world!");
+    let file_path = "test";
+    hash_safetensors_file(file_path.to_string());
+}
+
+fn hash_safetensors_file(file_path: String) {
+    println!("Hashing file: {file_path}");
 }
