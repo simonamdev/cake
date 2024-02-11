59ed3df47b82857be6e1512891dd7c7616580ccd
Simon Agius Muscat <simon@agius-muscat.net>
2024-02-12 00:27:00 +0100

    Complete result file creation with some very hacky path building

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
+use std::env;
 use std::fs::{self, File};
-use std::io::{self, BufReader, ErrorKind, Read, Seek, SeekFrom};
+use std::io::{self, BufReader, ErrorKind, Read, Seek, SeekFrom, Write};
 use std::convert::TryInto;
 use std::path::{Path, PathBuf};
 
-use serde_json::{json, value, Error, Value};
+use serde_json::{json, value, Error, Map, Value};
 use sha2::{Sha256, Digest};
 
 fn main() {
@@ -20,25 +21,49 @@ fn main() {
     // }
 
     let root_models_dir = "/media/simon/models3/results";
-    let model_directories = get_models_by_account(root_models_dir);
-    for (key, value) in model_directories {
-        println!("{}, {:?}", key, value);
-        for dir in value {
-            let model_safetensors = get_model_safetensor_files(&dir);
+    let model_directories_by_account = get_models_by_account(root_models_dir);
+    for (model_account, model_dirs) in model_directories_by_account {
+        println!("{}, {:?}", model_account, model_dirs);
+        for model_name_dir in model_dirs {
+            let model_safetensors = get_model_safetensor_files(&model_name_dir);
+            let model_name = model_name_dir.split("/").last().unwrap();
+            let mut hashed_model_safetensors: Vec<Value> = Vec::new();
             println!("{:?}", model_safetensors);
             for safetensors_file_path in model_safetensors {
                 let hashed_model_result = hash_safetensors_file(&safetensors_file_path.to_string_lossy().into_owned());
                 match hashed_model_result {
                     Ok(value) => {
-                        println!("{}", value);
+                        // println!("{}", value);
+                        hashed_model_safetensors.push(value);
                     }
                     Err(e) => {
                         eprintln!("{}", e);
                     }
                 }
             }
+            let final_output_json = merge_hash_results(hashed_model_safetensors);
+            println!("{:?}", final_output_json);
+            // Form the file path for the results
+            let model_account_clone = model_account.clone();
+            // Forgive me for this but pathbuf wasn't funcitoning as expected
+            let abs_dir: String =  env::current_dir().unwrap().to_string_lossy().into_owned().clone() + &"/results/".to_owned() + &model_account_clone.clone() + "/" + &model_name;
+            // Build the dir in case it doesnt exist
+            println!("==== {:?}", abs_dir);
+            let abs_dir_clone: String = abs_dir.clone();
+            fs::create_dir_all(abs_dir).unwrap();
+            let target_file_path = abs_dir_clone.to_owned() + "/hashes.json";
+
+            write_json_to_file(final_output_json, target_file_path).unwrap();
         }
     }
+}
+
+fn write_json_to_file(data: Value, file_path: String) -> io::Result<()> {
+    println!("Writing to: {:?}", file_path);
+    let json_string = serde_json::to_string_pretty(&data)?;
+    let mut file = File::create(file_path)?;
+    file.write_all(json_string.as_bytes())?;
+    Ok(())
 }
 
 fn get_model_safetensor_files(directory: &str) -> Vec<PathBuf> {
@@ -64,16 +89,18 @@ fn get_models_by_account(root_models_directory: &str) -> HashMap<String, Vec<Str
     let mut models_by_account_map: HashMap<String, Vec<String>> = HashMap::new();
 
     // First set of directories is the HF account
-    let owners = get_directories(root_models_directory);
+    let owners_dirs = get_directories(root_models_directory);
 
-    owners.into_iter().for_each(|owner| {
-        let owner_clone = owner.clone();
-        let mut model_dir = PathBuf::from(root_models_directory);
-        model_dir.push(owner);
-        let mut models = get_directories(&model_dir.to_string_lossy().into_owned());
-        let entry = models_by_account_map.entry(owner_clone).or_insert_with(Vec::new);
-        entry.append(&mut models);
-    });
+    owners_dirs.into_iter()
+        .map(|owner_dir| owner_dir.clone().split("/").last().unwrap().to_string())
+        .for_each(|owner| {
+            let owner_clone = owner.clone();
+            let mut model_dir = PathBuf::from(root_models_directory);
+            model_dir.push(owner);
+            let mut models = get_directories(&model_dir.to_string_lossy().into_owned());
+            let entry = models_by_account_map.entry(owner_clone.to_string()).or_insert_with(Vec::new);
+            entry.append(&mut models);
+        });
 
     models_by_account_map
 }
@@ -98,6 +125,28 @@ fn get_directories(directory: &str) -> Vec<String> {
     }
 
     return model_directories
+}
+
+fn merge_hash_results(json_results: Vec<Value>) -> Value {
+    // Setup new variables to hold the merged values
+    let mut file_paths: HashSet<String> = HashSet::new();
+    let mut merged_tensors: Map<String, Value> = Map::new();
+
+    for json_result in json_results {
+        let file_path = json_result.get("file_path").and_then(Value::as_str).unwrap();
+        file_paths.insert(file_path.to_string());
+        let tensors = json_result.get("tensors").and_then(Value::as_object).unwrap();
+        for (key, value) in tensors {
+            merged_tensors.entry(key.clone()).or_insert_with(|| value.clone());
+        }
+    }
+
+    let merged_json = json!({
+        "file_paths": file_paths,
+        "tensors": merged_tensors
+    });
+
+    return merged_json
 }
 
 fn hash_safetensors_file(file_path: &str) -> Result<Value, io::Error> {
@@ -152,10 +201,10 @@ fn hash_safetensors_file(file_path: &str) -> Result<Value, io::Error> {
                         file.read_exact(&mut tensor_buffer)?;
         
                         // Calculate SHA-256 hash of tensor data
-                        println!("{} / {} Hashing...", index, map.len());
+                        println!("{} / {} Hashing...", index+1, map.len());
                         let hash = sha256_hash(&tensor_buffer);
                         println!("SHA-256 Hash: {}", hash);
-                        output_object["layers"][key] = json!(hash);
+                        output_object["tensors"][key] = json!(hash);
                     }
                 }
             }
