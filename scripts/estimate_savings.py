307d1f6b6f2afbb19433c498b17e70c0b4d11b3f
Simon Agius Muscat <simon@agius-muscat.net>
2024-02-18 23:22:53 +0100

    Amend savings estimator to use new file format

--- scripts/estimate_savings.py
+++ scripts/estimate_savings.py
@@ -13,10 +13,10 @@ def calculate_byte_content(result_file_paths):
 
     for file_path in result_file_paths:
         data = read_json(file_path)
-        tensors = data.get('tensors', {})
-        for tensor_info in tensors.values():
-            layer_hash = tensor_info['hash']
-            byte_count = tensor_info['byte_count']
+        for tensor_metadata in data.values():
+            layer_hash = tensor_metadata['hash']
+            offsets = tensor_metadata['data_offsets']
+            byte_count = offsets[1] - offsets[0]
 
             total_byte_content += byte_count
 
@@ -24,7 +24,6 @@ def calculate_byte_content(result_file_paths):
                 saved_byte_content += byte_count
 
             tracked_layer_hashes.add(layer_hash)
-                
 
     num_files = len(result_file_paths)
 
@@ -42,19 +41,21 @@ def main():
     for account in accounts:
         models = os.listdir(os.path.join('../', 'results', account))
         for model in models:
-            result_file_paths.append(
-                os.path.join('../', 'results', account, model, 'hashes.json')
-            )
+            hashes_file_path = os.path.join('../', 'results', account, model, 'hashes.json')
+            if os.path.exists(hashes_file_path):
+                result_file_paths.append(
+                    hashes_file_path
+                )
 
     total_byte_content, num_files, bytes_saved, percent_saved = calculate_byte_content(result_file_paths)
 
     total_mb = bytes_to_megabytes(total_byte_content)
     bytes_saved_mb = bytes_to_megabytes(bytes_saved)
 
-    print("Total byte content across all files (MB):", total_mb)
     print("Number of files involved:", num_files)
-    print("Amount of bytes saved if duplicate layers are deduped (absolute) (MB):", bytes_saved_mb)
-    print("Amount of bytes saved if duplicate layers are deduped (percentage):", percent_saved, "%")
+    print("Total byte content across all files (MB):", round(total_mb, 2))
+    print("Amount of bytes saved if duplicate layers are deduped (absolute) (MB):", round(bytes_saved_mb, 2))
+    print("Amount of bytes saved if duplicate layers are deduped (percentage):", round(percent_saved, 2), "%")
 
 if __name__ == "__main__":
     main()
